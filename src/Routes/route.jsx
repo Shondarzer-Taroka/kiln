4137bbc86ad626a29621545c0cac3855d09f1b81
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2024-12-02 12:34:07 +0600

    tailwind css installed

---
+++ src/Routes/route.jsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from "react-router-dom";
+
+let route = createBrowserRouter([
+    {
+        path: '/',
+        element: 'hi',
+        children: [{
+        }]
+    }
+])
+
+export default route
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { RouterProvider } from 'react-router-dom'
+import route from './Routes/route.jsx'
 
 createRoot(document.getElementById('root')).render(
+  <RouterProvider router={route}>
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
+  </RouterProvider>,
 )
